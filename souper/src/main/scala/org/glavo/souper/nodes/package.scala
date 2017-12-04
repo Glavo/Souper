4ce7554bee0246a0c1c7673e65c7f7fa6201818a
Glavo <zjx001202@126.com>
2017-12-04 20:18:25 +0800

    rename project

--- souper/src/main/scala/org/glavo/souper/nodes/package.scala
+++ souper/src/main/scala/org/glavo/souper/nodes/package.scala
@@ -3,7 +3,61 @@ package org.glavo.souper
 import org.jsoup.{nodes => jn}
 
 package object nodes {
+
+  // RichNode
+
   implicit final class RichNode(val node: jn.Node) extends AnyVal {
+    @inline
     def asSouper: Node = Node(node)
   }
+
+  // RichElement
+
+  implicit final class RichElement(val node: jn.Element) extends AnyVal {
+    @inline
+    def asSouper: Element = Element(node)
+  }
+
+  implicit final class RichDocument(val node: jn.Document) extends AnyVal {
+    @inline
+    def asSouper: Document = Document(node)
+  }
+
+  implicit final class RichFormElement(val node: jn.FormElement) extends AnyVal {
+    @inline
+    def asSouper: FormElement = FormElement(node)
+  }
+
+  implicit final class RichPseudoTextElement(val node: jn.PseudoTextElement) extends AnyVal {
+    @inline
+    def asSouper: PseudoTextElement = PseudoTextElement(node)
+  }
+
+  // RichLeafNode
+
+  implicit final class RichComment(val node: jn.Comment) extends AnyVal {
+    @inline
+    def asSouper: Comment = Comment(node)
+  }
+
+  implicit final class RichDataNode(val node: jn.DataNode) extends AnyVal {
+    @inline
+    def asSouper: DataNode = DataNode(node)
+  }
+
+  implicit final class RichDocumentType(val node: jn.DocumentType) extends AnyVal {
+    @inline
+    def asSouper: DocumentType = DocumentType(node)
+  }
+
+  implicit final class RichTextNode(val node: jn.TextNode) extends AnyVal {
+    @inline
+    def asSouper: TextNode = TextNode(node)
+  }
+
+  implicit final class RichXmlDeclaration(val node: jn.XmlDeclaration) extends AnyVal {
+    @inline
+    def asSouper: XmlDeclaration = XmlDeclaration(node)
+  }
+
 }
